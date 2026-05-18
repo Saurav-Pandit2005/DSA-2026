447e886873d379724498760a87feba45b0803434
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-19 00:51:08 +0530

    first commit..

---
+++ Arrays(Part-1)/Array_Updation.java
@@ -0,0 +1,22 @@
+public class Array_Updation {
+
+    public static void update(int marks[], int nonchange){
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+
+        nonchange = 05;
+    }
+    public static void main(String[] args) {
+        int marks[] = {90,95,97};
+        int nonchange = 02;
+
+        update(marks, nonchange);
+        for(int i=0; i<marks.length; i++){
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print(nonchange);
+    }
+}
