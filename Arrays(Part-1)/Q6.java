41be74eda66777022d3c74bad562db81da9b1ea7
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-20 02:19:46 +0530

    Add Q6

---
+++ Arrays(Part-1)/Q6.java
@@ -0,0 +1,24 @@
+/*      Print Subarray   => a countinuous  part of array */
+
+
+public class Q6{
+    public static void subarray(int nums[]) {
+        int totalsubarray = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){
+                for(int k=i; k<=j; k++){
+                    System.out.print(nums[k] + " ");
+                }
+                totalsubarray++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total Subarray : " + totalsubarray);
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,4,6,8,10};
+
+        subarray(nums);
+    }
+}
