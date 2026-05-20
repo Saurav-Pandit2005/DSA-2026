cb924278bcc43b0e784466692f92175b9f47cdf8
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-20 21:35:17 +0530

    Add Q7.java

---
+++ Arrays(Part-1)/Q7.java
@@ -0,0 +1,29 @@
+/*      Max Sybarray Sum (Brute Force Approch)   */
+
+public class Q7 {
+
+    public static int max_subarray_sum(int nums[]) {
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){
+                currentsum = 0;
+                for(int k=i; k<=j; k++){
+                    currentsum = currentsum + nums[k];
+                    if(maxsum < currentsum){
+                        maxsum = currentsum;
+                    }
+                    System.out.println(currentsum);
+                }
+            }
+        }
+        return maxsum;
+    }
+    public static void main(String[] args) {
+        int nums[] = {10,20,30,5,10,50};
+
+        int maxiumsum = max_subarray_sum(nums);
+        System.out.println("Max Sumarray sum is : " + maxiumsum);
+    }
+}
