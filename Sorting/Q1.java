a096900a377c622e43f7a0efe133270b176e7e62
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-23 23:41:31 +0530

    Add Q1.java

---
+++ Sorting/Q1.java
@@ -0,0 +1,31 @@
+/*  
+    {Sorting : Arrange in an Order}
+    Bubble Sorting --> large elements come to the end of array by swapping with adjacent element 
+*/
+
+package Sorting;
+
+public class Q1 {
+    public static void BubbleSort(int nums[]){
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=0; j<nums.length-1-i; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void PrintArray(int nums[]){
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int nums[] = {10,2,15,16,4,11};
+        BubbleSort(nums);
+        PrintArray(nums);
+    }
+}
