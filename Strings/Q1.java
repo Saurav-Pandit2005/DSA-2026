196ce6062521f39897312de0dbba349600393316
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-25 20:12:08 +0530

    Add Q1.java

--- Strings/Q1.java
+++ Strings/Q1.java
@@ -1,7 +1,18 @@
+/*  Check if a String is Palindrome  */
+
 package Strings;
 
 public class Q1 {
+    public static boolean isPalindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        
+        String str = "madam";
+        System.out.println(isPalindrome(str));
     }
 }
