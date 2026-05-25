6d5310cba3940aabaf5fc018484ef2fc54333077
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-25 21:07:18 +0530

    String Basics..

--- Strings/String_Basic.java
+++ Strings/String_Basic.java
@@ -3,11 +3,11 @@ package Strings;
 import java.util.Scanner;
 
 public class String_Basic {
-    public static void printArr(String fullName) {
-        for(int i=0; i<fullName.length(); i++){
-            System.out.print(fullName.charAt(i) + " ");
-        }
-    }
+    // public static void printArr(String fullName) {
+    //     for(int i=0; i<fullName.length(); i++){
+    //         System.out.print(fullName.charAt(i) + " ");
+    //     }
+    // }
     public static void main(String[] args) {
         // char arr[] = {'c','e','w'};
 
@@ -28,14 +28,50 @@ public class String_Basic {
 
         // concatenation
 
-        String firstName = "Saurav";
-        String lastName = "Pandit";
+        // String firstName = "Saurav";
+        // String lastName = "Pandit";
 
-        String fullName = firstName + " " + lastName;
+        // String fullName = firstName + " " + lastName;
 
-        System.out.println(fullName);
+        // System.out.println(fullName);
 
-        printArr(fullName);
+        // printArr(fullName);
+
+        // Comapre function
+        
+        // String s1 = "Om";
+        // String s2 = "Om";
+        // String s3 = new String("Om");
+
+        // if(s1 == s2){
+        //     System.out.println("Strings are Equale");
+        // }else{
+        //     System.out.println("Strings are not Equale");
+        // }
+
+        // if(s1 == s3){
+        //     System.out.println("Strings are Equale");
+        // }else{
+        //     System.out.println("Strings are not Equale");
+        // }
+
+        // if(s1.equals(s3)){
+        //     System.out.println("Strings are Equale");
+        // }else{
+        //     System.out.println("Strings are not Equale");
+        // }
+
+        // Substring
+        // String str = "HelloWorld";
+        // System.out.println(str.substring(0,5));
+
+        // StringBuilder
+
+        StringBuilder sb = new StringBuilder("");
+        for(char ch ='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
 
         sc.close();
     }
