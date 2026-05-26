3584d04f107554d1617faa5bbc67ebfb5529df38
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-26 22:19:31 +0530

    Q9.java

--- Arrays(Part-1)/Q9.java
+++ Arrays(Part-1)/Q9.java
@@ -1,5 +1,21 @@
+/*      Max Sybarray Sum (Kadane's Algorithm Approch)   */
+
 public class Q9{
+    public static int max_subarraysum(int nums[]){
+        int currentsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for(int i=0; i<nums.length; i++) {
+            currentsum+= nums[i];
+            maxsum = Math.max(currentsum, maxsum);
+            if(currentsum < 0) {
+                currentsum = 0;
+            }
+        }
+        return maxsum;
+    }
     public static void main(String[] args) {
-        
+        int nums[] = {-2,-3,4,-1,-2,1,5,-3};
+        System.out.println(max_subarraysum(nums));
     }
 }
