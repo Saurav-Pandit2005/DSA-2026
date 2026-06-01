d012187d354d52003e91aed29bc73309972210ac
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-01 13:05:43 +0530

    add basic file

--- Hashing/Hashing_Basic.java
+++ Hashing/Hashing_Basic.java
@@ -1,6 +1,5 @@
 package Hashing;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class Hashing_Basic {
     public static void main(String[] args) {
@@ -36,22 +35,42 @@ public class Hashing_Basic {
         // // Is Empty
         // System.out.println(hm.isEmpty());
 
-        HashMap<String, Integer> hm = new HashMap<>();
+        // HashMap<String, Integer> hm = new HashMap<>();
 
-        hm.put("India", 2000);
-        hm.put("China", 2500);
-        hm.put("Russia", 3000);
-        hm.put("US", 4000);
+        // hm.put("India", 2000);
+        // hm.put("China", 2500);
+        // hm.put("Russia", 3000);
+        // hm.put("US", 4000);
 
-        System.out.println(hm);
+        // System.out.println(hm);
 
-        // Iteration on HashMap
+        // // Iteration on HashMap
 
-        Set<String> keys = hm.keySet();         // Creating Set for Iteration
-        System.out.println(keys);
+        // Set<String> keys = hm.keySet();         // Creating Set for Iteration
+        // System.out.println(keys);
 
-        for (String k : keys) {
-            System.out.println("key = " + k + "," + "value = " + hm.get(k));
-        }
+        // for (String k : keys) {
+        //     System.out.println("key = " + k + "," + "value = " + hm.get(k));
+        // }
+
+
+        // LinkedHashMap
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+
+        lhm.put("India", 2000);
+        lhm.put("Brazil", 1200);
+        lhm.put("Ukrain", 2200);
+
+        System.out.println(lhm);
+
+        // TreeMap
+        TreeMap<String, Integer> tm = new TreeMap<>();
+
+        tm.put("Aus", 3600);
+        tm.put("India", 2000);
+        tm.put("Brazil", 3500);
+        tm.put("Chine", 6000);
+
+        System.out.println(tm);
     }
 }
