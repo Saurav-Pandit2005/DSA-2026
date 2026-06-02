dc31585f84dbd7b12f8b0d40cf417de14a634dcd
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-03 01:03:43 +0530

    Complete Q1.java

--- Hashing/Q1.java
+++ Hashing/Q1.java
@@ -12,10 +12,32 @@
  */
 
 package Hashing;
-
+import java.util.*;;
 public class Q1 {
 
+    public static void majority_element(int nums[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            // if(map.containsKey(nums[i])) {
+            //     map.put(nums[i], map.get(nums[i])+1);
+            // } else {
+            //     map.put(nums[i], 1);
+            // }
+
+            // OR
+
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(Integer key : map.keySet()) {
+            if(map.get(key) > nums.length/3) {
+                System.out.println(key);
+            }
+        }
+    }
     public static void main(String[] args) {
-        int nums[] = {1,3,2,5,1,3,1,5,1};;
+        int nums[] = {1,3,2,5,1,3,1,5,1};
+        majority_element(nums);
     }
 }
