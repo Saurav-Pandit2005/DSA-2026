c60d7adad9cbaae6e288ea5d7c9688fa0071be0c
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-02 21:20:39 +0530

    Add Q1

---
+++ Hashing/Q1.java
@@ -0,0 +1,21 @@
+/*
+    Given an integer array of size n, find all elements that appear more than [n/3] times.
+
+    Example :
+
+    nums[] = {1,3,2,5,1,3,1,5,1};
+    // 1
+    
+    nums[] = {1,2};
+    // 1 2
+
+ */
+
+package Hashing;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        int nums[] = {1,3,2,5,1,3,1,5,1};;
+    }
+}
