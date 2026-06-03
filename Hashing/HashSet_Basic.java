19cdc2a78b5b8ce83ed26d80c23931b2297ace40
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-03 22:16:12 +0530

    Update Set basic

--- Hashing/HashSet_Basic.java
+++ Hashing/HashSet_Basic.java
@@ -3,37 +3,53 @@ import java.util.*;
 public class HashSet_Basic {
     public static void main(String[] args) {
         // Create a HashSet
-        HashSet <Integer> set =  new HashSet<>();
+        // HashSet <Integer> set =  new HashSet<>();
 
         // Operations on HashSet
 
         // add
-        set.add(2);
-        set.add(4);
-        set.add(10);
+        // set.add(2);
+        // set.add(4);
+        // set.add(10);
 
-        System.out.println(set);
+        // System.out.println(set);
 
         // remove
-        set.remove(4);
-        System.out.println(set);
+        // set.remove(4);
+        // System.out.println(set);
 
         // containsKey
-        if(set.contains(2)) {
-            System.out.println("Set Contains 2");
-        }
+        // if(set.contains(2)) {
+        //     System.out.println("Set Contains 2");
+        // }
 
         // size()
 
-        System.out.println(set.size());
+        // System.out.println(set.size());
 
         // clear()
 
-        set.clear();
-        System.out.println(set);
+        // set.clear();
+        // System.out.println(set);
 
         // isEmpty()
 
-        System.out.println(set.isEmpty());
+        // System.out.println(set.isEmpty());
+
+        HashSet<String> cities = new HashSet<>();
+
+        cities.add("Mumbai");
+        cities.add("Rajkot");
+        cities.add("Hydrabad");
+        cities.add("Chennai");
+
+        // Iterator it = cities.iterator();
+        // while(it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
+        // for(String city : cities) {
+        //     System.out.println(city);
+        // }
     }
 }
