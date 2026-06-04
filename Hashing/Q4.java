415cbf0ee8b88716083932fe00f11e1151cdf16d
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-04 22:37:21 +0530

    add Q4.java

--- Hashing/Q4.java
+++ Hashing/Q4.java
@@ -1,3 +1,11 @@
+// Union & Intersection of 2 arrays
+/*
+    arr1 = {7,3,9}
+    arr2  {6,3,9,2,9,4}
+
+    union = 6 (7,3,9,6,2,4)
+    intersection = 2 (3,9)
+*/
 package Hashing;
 
 public class Q4 {
