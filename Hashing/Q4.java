17964583e480e38ad19b047a9148547a74c7945b
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-04 23:05:00 +0530

    perform Union

--- Hashing/Q4.java
+++ Hashing/Q4.java
@@ -8,6 +8,25 @@
 */
 package Hashing;
 
+import java.util.HashSet;
+
 public class Q4 {
-    
+    public static void main(String[] args) {
+        int[] arr1 = {7,3,9};
+        int[] arr2 = {6,3,9,2,9,4};
+
+        // Union
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(Integer num : arr1) {
+            set.add(num);
+        }
+
+        for(Integer num : arr2) {
+            set.add(num);
+        }
+
+        System.out.println(set.size());
+    }
 }
