8ac8fe57dee7a6d67afcdf20a2449a8acc52d733
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-04 22:19:21 +0530

    update Q3

--- Hashing/Q3.java
+++ Hashing/Q3.java
@@ -1,5 +1,11 @@
 // Count Distinct Elements
 
+/*
+    int nums[] = {4,3,2,5,6,7,3,4,2,1};
+    ans = 7
+
+    -> Bcz here 7 distinct element {4,3,2,5,6,7,1}
+*/
 package Hashing;
 
 import java.util.HashSet;
