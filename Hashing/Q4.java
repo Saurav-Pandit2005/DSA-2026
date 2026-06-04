84cacca48654b8d4a238b64b9aed8b50db1857f7
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-04 23:14:29 +0530

    add intersection part

--- Hashing/Q4.java
+++ Hashing/Q4.java
@@ -28,7 +28,24 @@ public class Q4 {
         }
 
         System.out.println(set + " = " + set.size());
-        // System.out.println(set.size());
+        System.out.println(set.size());
 
+        // Intersection
+
+        set.clear();
+
+        for(int i=0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+
+        for(int i=0; i<arr2.length; i++) {
+            if(set.contains(arr2[i])) {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        System.out.println(count);
     }
 }
