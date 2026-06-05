f9dd7f750a36a96801f3d44964e211482c9d4f09
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-05 17:23:08 +0530

    add Q3.java

---
+++ ArrayList/Q3.java
@@ -0,0 +1,39 @@
+// Swap 2 Numbers
+
+/*
+    list = 2,5,9,3,6      index : idx1 = 1, idx2 = 3
+*/
+
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Q3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        System.out.println(list);
+
+        // int temp, idx1 = 1, idx2 = 3;
+
+        // temp = list.get(idx1);
+        // list.set(idx1, list.get(idx2));
+        // list.set(idx2, temp);
+
+        // OR
+
+        int temp;
+
+        temp = list.get(1);
+        list.set(1, list.get(3));
+        list.set(3, temp);
+
+        System.out.println(list);
+    }
+}
