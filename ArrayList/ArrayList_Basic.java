bd0aa4e4ef5ae9ed8dab1b576b3decebbe8c9e77
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-05 13:01:44 +0530

    add .basic file

--- ArrayList/ArrayList_Basic.java
+++ ArrayList/ArrayList_Basic.java
@@ -1,7 +1,13 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayList_Basic {
     public static void main(String[] args) {
-        
+
+        // Creating a ArrayList
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+ 
     }
 }
