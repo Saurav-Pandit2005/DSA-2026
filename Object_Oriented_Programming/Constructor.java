6f82e02ddcb5e0c5c7d1cc8c76ab8c737cbf6e71
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-06 20:12:40 +0530

    update const....

--- Object_Oriented_Programming/Constructor.java
+++ Object_Oriented_Programming/Constructor.java
@@ -6,14 +6,38 @@ class Student {
     String name;
     int age;
 
-    Student(String name) {
-        this.name = name;
-    }
+    // Default Constructor
+    // Student() {
+    //     System.out.println("Constructor is called....!");
+    // }
+
+    // Parametrazied Constructor
+    // Student(String name) {
+    //     this.name = name;
+    // }
+
+    // Student(int age) {
+    //     this.age = age;
+    // }
+
+    // Copy Constructor
 }
 
 public class Constructor {
     public static void main(String[] args) {
-        Student s1 = new Student("Saurav");
-        System.out.println(s1.name);
+
+        // Default Constructor
+        // Student s1 = new Student();
+        // System.out.println(s1);
+
+        // Parametrazied Constructor
+        // Student s2 = new Student("Saurav");
+        // System.out.println(s2.name);
+
+        // Student s3 = new Student(20);
+        // System.out.println(s3.age);
+
+        // Copy Constructor
+        
     }
 }
