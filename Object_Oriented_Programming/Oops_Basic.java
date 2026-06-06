fbe116680bf78163d71f6ed4c187390cd34679a2
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-06 11:38:41 +0530

    update .basic oops

--- Object_Oriented_Programming/Oops_Basic.java
+++ Object_Oriented_Programming/Oops_Basic.java
@@ -3,27 +3,35 @@ package Object_Oriented_Programming;
 class Pen {
     // Properties + Function
 
-    String color;
-    int tip;
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
 
     void setColor(String newColor) {
-        color = newColor;
+        this.color = newColor;
     }
 
     void setTip(int newTip) {
-        tip = newTip;
+        this.tip = newTip;
     }
 }
 
-class Student {
-    String name;
-    int age;
-    float cgpa;
+// class Student {
+//     String name;
+//     int age;
+//     float cgpa;
 
-    void calcCgpa(int phy, int chem, int math) {
-        cgpa = phy + chem + math / 3;
-    }
-}
+//     void calcCgpa(int phy, int chem, int math) {
+//         cgpa = phy + chem + math / 3;
+//     }
+// }
 
 // class BankAccount {
 //     public String username;
@@ -40,16 +48,13 @@ public class Oops_Basic {
         p1.setColor("pink");
         p1.setTip(7);
 
-        System.out.println(p1.color);
-        System.out.println(p1.tip);
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
 
         // BankAccount Class
 
         // BankAccount myAccount = new BankAccount();
         // myAccount.username = "SauravPandit";
         // myAccount.setPassword("abc");
-
-        
     }
 }
-
