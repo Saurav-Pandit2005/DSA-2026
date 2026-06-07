46925cc436c3ab9f7a40661388c5cec165216424
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-07 09:38:14 +0530

    update Q5

--- Hashing/Q5.java
+++ Hashing/Q5.java
@@ -1,9 +1,54 @@
 // Find itinerary for Tickets
 
+/*
+    "Chennai" ->  "Bengaluru"
+    "Mumbai"  ->  "Delhi"
+    "Goa"     ->  "Chennai"
+    "Delhi"   ->  "Goa"
+
+    Output :
+    -----------
+
+    "Mumbai" -> "Delhi" -> "Goa" -> "Chennai" -> "Bengaluru"
+*/
+
+//  O(n)    -> Time Complexity
+
 package Hashing;
 
+import java.util.HashMap;
+
 public class Q5 {
-    public static void main(String[] args) {
+
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> revMap = new HashMap<>();
         
+        for(String key : tickets.keySet()) {
+            revMap.put(tickets.get(key), key);
+        }
+
+        for(String key : tickets.keySet()) {
+            if(!revMap.containsKey(key)) {
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+        
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+        System.out.print(start);
+        for(String key : tickets.keySet()) {
+            System.out.print(" -> " + tickets.get(start));
+            start = tickets.get(start);
+        }
+        System.out.println();
     }
 }
