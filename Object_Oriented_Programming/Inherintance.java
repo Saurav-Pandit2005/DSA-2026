180cf94f1b03d00a181848afdb56fc9b4ea30327
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-08 13:04:40 +0530

    update inheri....

--- Object_Oriented_Programming/Inherintance.java
+++ Object_Oriented_Programming/Inherintance.java
@@ -68,33 +68,99 @@ package Object_Oriented_Programming;
 
 // Hierarchical Inheritance
 
-class Animal {
-    String color;
+// class Animal {
+//     String color;
 
-    void animal() {
-        System.out.println("Animals..");
-    }
-}
+//     void animal() {
+//         System.out.println("Animals..");
+//     }
+// }
 
-class Dog extends Animal {
-    void bark() {
-        System.out.println("Bark..");
-    }
-}
+// class Dog extends Animal {
+//     void bark() {
+//         System.out.println("Bark..");
+//     }
+// }
 
-class Cat extends Animal {
-    void meow() {
-        System.out.println("Meowww....");
-    }
-}
+// class Cat extends Animal {
+//     void meow() {
+//         System.out.println("Meowww....");
+//     }
+// }
 
-public class Inherintance {
-    public static void main(String[] args) {
+// public class Inherintance {
+//     public static void main(String[] args) {
 
-        Dog d1 = new Dog();
-        d1.bark();
+//         Dog d1 = new Dog();
+//         d1.bark();
 
-        Cat c1 = new Cat();
-        c1.meow();
-    }
-}
+//         Cat c1 = new Cat();
+//         c1.meow();
+//     }
+// }
+
+
+// ------------------------------------------------------------
+
+// Hybrid Inherintance
+
+// class Animal {
+//     void animal() {
+//         System.out.println("Animal..");
+//     }
+// }
+
+// class Fish extends Animal {
+//     void fish() {
+//         System.out.println("fish..");
+//     }
+// }
+
+// class Shark extends Fish {
+//     void shark() {
+//         System.out.println("Shark..");
+//     }
+// }
+
+// class Whale extends Fish {
+//     void whale() {
+//         System.out.println("Whale..");
+//     }
+// }
+
+// class Bird extends Animal {
+//     void bird() {
+//         System.out.println("Bird..");
+//     }
+// }
+
+// class Mammle extends Animal {
+//     void mammle() {
+//         System.out.println("Mammle..");
+//     }
+// }
+
+// class Dog extends Animal {
+//     void dog() {
+//         System.out.println("Dog..");
+//     }
+// }
+
+// class Cat extends Mammle {
+//     void cat() {
+//         System.out.println("Cat..");
+//     }
+// }
+
+// class Human extends Mammle {
+//     void human() {
+//         System.out.println("Human...");
+//     }
+// }
+
+// public class Inherintance {
+//     public static void main(String[] args) {
+//         Human h1 = new Human();
+//         h1.human();
+//     }
+// }
