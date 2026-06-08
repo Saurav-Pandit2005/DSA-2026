c6868e6e2c0a5c04c805ec9939c0a0d38b9992d0
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-09 00:52:24 +0530

    add Q1.java

---
+++ Arrays_2D/Q1.java
@@ -0,0 +1,23 @@
+// Search in 2D Matrix
+
+package Arrays_2D;
+
+public class Q1 {
+
+    public static boolean search_Matrix(int[][] matrix, int target) {
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+ 
+    public static void main(String[] args) {
+        int matrix[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 300;
+        System.out.println(search_Matrix(matrix, target));
+    }
+}
