77393f15e32e97ad335bffaa13068838de6fea4b
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-09 00:26:01 +0530

    update Q1

--- Arrays(Part-1)/Q1.java
+++ Arrays(Part-1)/Q1.java
@@ -1,24 +1,24 @@
 /*     linear Search     */
 
-public class Q1{
+// public class Q1{
 
-    public static int linearSearch(int numbers[], int target){
-        for(int i=0; i<numbers.length; i++){
-            if(numbers[i] == target){
-                return i;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        int numbers[] = {2,4,7,8,17,20,14};
-        int target = 17;
+//     public static int linearSearch(int numbers[], int target){
+//         for(int i=0; i<numbers.length; i++){
+//             if(numbers[i] == target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int numbers[] = {2,4,7,8,17,20,14};
+//         int target = 17;
 
-        int result = linearSearch(numbers, target);
-        if(result == -1){
-            System.out.println("Element not found in the Array");
-        }else{
-            System.out.println("Element found at index : " + result);   
-        }
-    }
-}
+//         int result = linearSearch(numbers, target);
+//         if(result == -1){
+//             System.out.println("Element not found in the Array");
+//         }else{
+//             System.out.println("Element found at index : " + result);   
+//         }
+//     }
+// }
