f74e2d768086ee946a3cf517bd4f7006387a46aa
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-08 13:19:46 +0530

    add polymorphism

---
+++ Object_Oriented_Programming/Polymorphism.java
@@ -0,0 +1,30 @@
+package Object_Oriented_Programming;
+
+//   Compile-Time Polymorphism (Method Overloading)
+        
+// class Calculator {
+//     int sum(int a, int b) {
+//         return a+b;
+//     }
+
+//     float sum(float a, float b) {
+//         return a+b;
+//     }
+
+//     int sum(int a, int b, int c) {
+//         return a+b+c;
+//     }
+// }
+
+// public class Polymorphism {
+//     public static void main(String[] args) {
+//         Calculator c1 = new Calculator();
+//         System.out.println(c1.sum(20, 30));
+//         System.out.println(c1.sum((float)31.5, (float)18.5));
+//         System.out.println(c1.sum(20, 30, 50));
+//     }
+// }
+
+// -----------------------------------------------------------------------------------
+
+// Runtime Polymorphism (Method Overriding)
