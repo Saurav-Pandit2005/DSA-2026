9f43f32f82e75e3015454186f34be88f63885cc1
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-10 12:58:28 +0530

    add Q2.java

---
+++ Arrays_2D/Q2.java
@@ -0,0 +1,10 @@
+// Spiral Matrix
+
+package Arrays_2D;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3,4},{10,11,16,20},{23,30,34,60}};
+        
+    }
+}
