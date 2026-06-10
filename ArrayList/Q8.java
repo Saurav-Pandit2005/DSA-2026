2d4c2c44dd4cdb0355d75e2e8fa98928e255a156
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-10 12:56:32 +0530

    add Q8.java

---
+++ ArrayList/Q8.java
@@ -0,0 +1,14 @@
+// Pair Sum - 1 (Brute Force O(n2))
+
+/*
+    Find if any pair in a sorted ArrayList has a target sum.
+
+    list = [1,2,3,4,5,6]    target = 5;
+*/
+package ArrayList;
+
+public class Q8 {
+    public static void main(String[] args) {
+        
+    }
+}
