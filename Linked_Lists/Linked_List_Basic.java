669c13220ae3b277fcacafbafbdaff7044fa26d7
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-11 21:27:29 +0530

    add ll_basic

---
+++ Linked_Lists/Linked_List_Basic.java
@@ -0,0 +1,15 @@
+package Linked_Lists;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Linked_List_Basic {
+    
+}
