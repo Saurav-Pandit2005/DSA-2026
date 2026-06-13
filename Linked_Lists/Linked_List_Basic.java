0799d789d568a1d6108960cce50ed8932fef3a1d
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-13 13:13:24 +0530

    update .basic file

--- Linked_Lists/Linked_List_Basic.java
+++ Linked_Lists/Linked_List_Basic.java
@@ -1,15 +1,25 @@
 package Linked_Lists;
+public class Linked_List_Basic {
 
-class Node {
-    int data;
-    Node next;
+    public static class Node {
+        int data;
+        Node next;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static void main(String[] args) {
+        Linked_List_Basic ll = new Linked_List_Basic();
+
+        // for understanding ....
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
     }
 }
 
-public class Linked_List_Basic {
-    
-}
+
