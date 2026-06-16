720939f0ec270bba6abf3df503d580ac96b6cc6e
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-16 22:49:18 +0530

    add ll.basic

--- Linked_Lists/Linked_List_Basic.java
+++ Linked_Lists/Linked_List_Basic.java
@@ -13,12 +13,25 @@ public class Linked_List_Basic {
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data) {
+        // step1 = create new node
+        Node newNode = new Node(data);
+
+        // step2 = newNode next = head
+        newNode.next = head; // link
+
+        
+    }
+
+
     public static void main(String[] args) {
         Linked_List_Basic ll = new Linked_List_Basic();
 
         // for understanding ....
         // ll.head = new Node(1);
         // ll.head.next = new Node(2);
+
+
     }
 }
 
