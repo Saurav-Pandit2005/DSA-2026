41c7b5d74d1119a93fc160518adb574909f081c9
Saurav Pandit <sauravpandit9691@gmail.com>
2026-06-26 11:13:01 +0530

    Add Q4.java

---
+++ Bit_Manipulation/Q4.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class Q4 {
+    public static int Cleat_ith_Bit(int n, int i) {
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        int n = 10, i = 1;
+        System.out.println(Cleat_ith_Bit(n, i));
+    }
+}
