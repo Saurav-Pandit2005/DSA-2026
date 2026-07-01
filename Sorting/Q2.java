8c8b5a6ba27ae30c2d3d5ad0a3de541077bf27dc
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-01 18:17:17 +0530

    Update .txt

--- Sorting/Q2.java
+++ Sorting/Q2.java
@@ -3,7 +3,29 @@
 package Sorting;
 
 public class Q2 {
+    public static void SelectionSort(int nums[]){
+        for(int i=0; i<nums.length-1; i++) {
+            int Smallest = i;
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[Smallest] > nums[j]) {
+                    Smallest = j;
+                }
+                // Swap
+                int temp = nums[Smallest];
+                nums[Smallest] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+    public static void PrintArray(int nums[]){
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int nums[] = {10,2,15,16,4,11};
+        SelectionSort(nums);
+        PrintArray(nums);
     }
 }
