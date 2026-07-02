154fe5a87ad434141f3906f541911d48f453c2b4
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-02 23:03:03 +0530

    Add Q3.java

---
+++ Recursion/Q3.java
@@ -0,0 +1,16 @@
+// Find Factorial of N
+
+package Recursion;
+
+public class Q3 {
+    public static int Factorial(int n) {
+        if(n == 0) return 1;
+        int fnm1 = Factorial(n-1);
+        int fn = n * fnm1;
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Factorial(n));
+    }
+}
