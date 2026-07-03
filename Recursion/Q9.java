fd63877e5c289afeed208977734452a5a6642500
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-03 22:42:29 +0530

    Add Q9.java

---
+++ Recursion/Q9.java
@@ -0,0 +1,24 @@
+// Print x^n
+
+// Tc = O(n)    Sc = O(n)
+
+package Recursion;
+
+public class Q9 {
+    public static int Power(int x, int n) {
+        if(n == 0) return 1;
+
+        int fnm1 = Power(x, n-1);
+        int fn = x * fnm1;
+        return fn; 
+
+        //  OR
+
+        // return x * Power(x, n-1);
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 10;
+        System.out.println(Power(x, n));
+    }
+}
