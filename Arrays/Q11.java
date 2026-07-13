9e4d818e8f32d212acf4ee884513e60b5cf75e94
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-13 21:39:36 +0530

    update Q11.java

--- Arrays/Q11.java
+++ Arrays/Q11.java
@@ -3,7 +3,23 @@
 package  Arrays;
 
 public class Q11 {
+    public static int BuySellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
     public static void main(String[] args) {
-        
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(BuySellStocks(prices));
     }
 }
