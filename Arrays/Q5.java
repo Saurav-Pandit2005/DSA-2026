f255c2dae153d526717d6a00931c776c19d0043b
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-13 14:15:14 +0530

    Update Q5.java

---
+++ Arrays/Q5.java
@@ -0,0 +1,24 @@
+/*    Pairs in Array   */
+
+package  Arrays;
+
+// Total number of pairs formula : n*(n-1)/2
+
+public class Q5 {
+    public static void pairs(int nums[]) {
+        int totalpair = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                System.out.print("(" + nums[i] + "," + nums[j] + ")" + " ");
+                totalpair++;
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Total Pairs : " + totalpair);
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,4,6,8,10};
+
+        pairs(nums);
+    }
+}
