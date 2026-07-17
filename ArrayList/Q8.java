b2f8db8fe6fe88de19380798ce8902f3c97e2547
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-17 20:45:33 +0530

    Add Q8.java

--- ArrayList/Q8.java
+++ ArrayList/Q8.java
@@ -1,4 +1,4 @@
-// Pair Sum - 1 (Brute Force O(n2))
+// Pair Sum - 1 (2 Pointer Apporach O(n))
 
 /*
     Find if any pair in a sorted ArrayList has a target sum.
@@ -7,8 +7,28 @@
 */
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Q8 {
+    public static boolean pairSum(ArrayList<Integer> list, int target) {
+        int start = 0, end = list.size()-1;
+        while(start < end) {
+            if(list.get(start) + list.get(end) == target) return true;
+            else if(list.get(start) + list.get(end) < target) start++;
+            else end--;
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int target = 5;
+        System.out.println(pairSum(list, target));
     }
 }
