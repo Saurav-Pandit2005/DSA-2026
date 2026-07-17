8dc5809035461b4877d26b2f9c1ded1d6d065e89
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-17 16:43:57 +0530

    Add Q4.java

--- Arrays_2D/Q4.java
+++ Arrays_2D/Q4.java
@@ -3,7 +3,28 @@
 package Arrays_2D;
 
 public class Q4 {
+    // Using Linear Search O(n^2) => Check Q1.java
+
+    // StairCase Search - O(n + m)
+    public static boolean Search_matrix(int matrix[][], int target) {
+        int row = 0, column = matrix[0].length-1;
+        while (row < matrix.length && column >= 0) {
+            if(matrix[row][column] == target) return true;
+            else if (matrix[row][column] < target) row++;
+            else column--;
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        
+        int[][] matrix = {
+                            {1, 2, 3, 4},
+                            {5, 6, 7, 8},
+                            {9, 10, 11, 12},
+                            {13, 14, 15, 16}
+                        };
+
+        int target = 10;
+
+        System.out.println(Search_matrix(matrix, target));
     }
 }
