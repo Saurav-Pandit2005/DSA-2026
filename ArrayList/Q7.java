ba310c3e350d1e9daedf11b6efef8f21c7a0ec47
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-17 21:21:58 +0530

    Update Q7.java

--- ArrayList/Q7.java
+++ ArrayList/Q7.java
@@ -11,7 +11,7 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class Q7 {
-    public static boolean pairSum(ArrayList<Integer> list, int target) {
+    public static boolean PairSum_1(ArrayList<Integer> list, int target) {
         for(int i=0; i<list.size(); i++) {
             for(int j=i+1; j<list.size(); j++) {
                 if(list.get(i) + list.get(j) == target) {
@@ -31,6 +31,6 @@ public class Q7 {
         list.add(5);
 
         int target = 5;
-        System.out.println(pairSum(list, target));
+        System.out.println(PairSum_1(list, target));
     }
 }
