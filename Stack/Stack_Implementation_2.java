6179eb0fbf34c1528dfcdd4c992a96b1348f24a2
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-26 16:00:42 +0530

    Add Imple_2

---
+++ Stack/Stack_Implementation_2.java
@@ -0,0 +1,72 @@
+// Stack Implementation Using LinkedList
+
+package Stack;
+
+public class Stack_Implementation_2 {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head = null;
+
+        // isEmpty()
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // push
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if(isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // pop
+
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // peek
+
+        public static int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(100);
+        s.push(200);
+        s.push(300);
+
+        while (! s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
