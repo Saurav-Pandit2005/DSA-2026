b7dd837273463ea41d289b81970d65204496ce2d
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-30 11:23:58 +0530

    Add Q2.java

---
+++ Linked_Lists/Q2.java
@@ -0,0 +1,28 @@
+// Search (Recursive)     - O(n)
+
+/*
+
+    Search for a key in a Linked List. Return the position where it is found. if not found, return -1.
+
+*/
+
+package Linked_Lists;
+
+public class Q2 {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    public static void main(String[] args) {
+        
+    }
+}
