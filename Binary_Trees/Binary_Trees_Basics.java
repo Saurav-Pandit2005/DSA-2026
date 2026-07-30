1d22d1e0234ad6caff46d738347f687d2f99dc9f
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-30 12:07:59 +0530

    Add B_Trees_Basics.java

---
+++ Binary_Trees/Binary_Trees_Basics.java
@@ -0,0 +1,44 @@
+// Build Tree Preorder Code
+
+package Binary_Trees;
+
+public class Binary_Trees_Basics {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if(nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+        // Build Tree Preorder function
+
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);     
+    }
+}
